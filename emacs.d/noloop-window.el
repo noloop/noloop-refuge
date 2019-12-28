6c8e6afb4e3963cc6329f093c1340c61930e9555
noloop <noloop@zoho.com>
2019-12-28 09:20:42 -0300

    New control z command disable hell sleep emacs.

--- emacs.d/noloop-window.el
+++ emacs.d/noloop-window.el
@@ -7,6 +7,12 @@
 (when (version<= "26.0.50" emacs-version)
   (global-display-line-numbers-mode))
 
+;; Disable Sleep Button
+(global-unset-key [(control z)])
+(global-unset-key [(control x)(control z)])
+;; Enable windows Style Undo
+(global-set-key [(control z)] 'undo)
+
 ;;(add-to-list 'load-path "~/.emacs.d/noloop-emacs-conf/contrib/")
 ;;(load "~/.emacs.d/noloop-emacs-conf/contrib/emacs-application-framework/eaf.el")
 
